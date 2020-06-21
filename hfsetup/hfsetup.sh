8123e010f45dffd8a56d460d999c2277be284978
David <d.satomi@outlook.de>
2020-06-21 15:51:35 +0200

    docker compose

--- hfsetup/hfsetup.sh
+++ hfsetup/hfsetup.sh
@@ -40,7 +40,7 @@ sudo apt-get update
 sudo apt-get install docker-ce
 
 # Install docker-compose
-sudo curl -L https://github.com/docker/compose/releases/download/1.26.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo curl -L https://github.com/docker/compose/releases/download/1.25.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 
 
 

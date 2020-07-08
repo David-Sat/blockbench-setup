1c82d110cba054bf88f16495d24c6060d3232733
David <d.satomi@outlook.de>
2020-07-09 00:19:03 +0200

    benchmark script preview

--- benchmark/benchmark.sh
+++ benchmark/benchmark.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+ordereraddr="localhost:7041"
+peeraddr="localhost:7051"
 
 helpFunction()
 {
@@ -10,6 +12,58 @@ helpFunction()
     echo -e "\t-x Description of txrate"
     echo -e "\t-t Description of threads"
     exit 1 # Exit script after printing help
+}
+
+startNetwork()
+{
+    echo "Spinning up four-nodes Fabric Network"
+    cd ~/blockbench/benchmark/fabric-v1.4/four-nodes-docker
+    docker-compose -f docker-compose.yaml up -d
+    echo "Create and join channel for each peer"
+    ./setup.sh
+}
+
+configureWorkload()
+{
+    echo "Configuring workload and deploying chaincode"
+    case $benchmark in
+    kvstore)
+        cd ~/blockbench/src/macro/kvstore/fabric-v1.4-node
+        npm install
+        ./deploy_kv.sh
+        # temporary solution:
+        ./driver -db fabric-v1.4 -threads 16 -P workloads/workloada.spec -txrate 100 -endpoint localhost:7041,localhost:7051 -wl ycsb
+        ;;
+    donothing)
+        cd ~/blockbench/src/macro/kvstore/fabric-v1.4-node
+        npm install
+        ./deploy_donothing.sh
+        # temporary solution:
+        ./driver -db fabric-v1.4 -threads 16 -P workloads/workloada.spec -txrate 100 -endpoint localhost:7041,localhost:7051 -wl donothing
+        ;;
+    smallbank)
+        cd ~/blockbench/src/macro/smallbank/api_adapters/fabric-v1.4-node
+        npm install
+        ./deploy.sh
+        # temporary solution:
+        ./driver  -db fabric-v1.4 -ops 1000 -threads 4 -txrate 100 -fp stat.txt -endpoint localhost:7041,localhost:7051
+        ;;
+    cpuheavy)
+        cd ~/blockbench/src/micro
+        npm install
+        cd ~/blockbench/src/micro/cpuheavy/fabric-v1.4
+        # TODO: add ./benchmark.sh <array_size>
+        ;;
+    ioheavy)
+        cd ~/blockbench/src/micro
+        npm install
+        cd ~/blockbench/src/micro/ioheavy/fabric-v1.4
+        # TODO: add write and scan or seperate micro into own script
+        ;;
+    *)
+        echo "Sorry, could not recognize benchmark"
+        ;;
+esac
 }
 
 while getopts "b:w:x:t:" opt
@@ -30,87 +84,12 @@ then
     helpFunction
 fi
 
+
 # Begin script in case all parameters are correct
 echo "$benchmark"
 echo "$workload"
 echo "$txrate"
 echo "$threads"
 
-case $benchmark in
-    kvstore)
-        echo "kvstore"
-        kvstoreFunction
-        ;;
-    donothing)
-        echo "donothing"
-        ;;
-    smallbank)
-        echo "smallbank"
-        ;;
-    cpuheavy)
-        echo "cpuheavy"
-        ;;
-    ioheavy)
-        echo "ioheavy"
-        ;;
-    *)
-        echo "Sorry, could not recognize benchmark"
-        ;;
-esac
-
-echo "Yup"
-
-startNetwork()
-{
-    echo "Spinning up four-nodes Fabric Network"
-    cd ~/blockbench/benchmark/fabric-v1.4/four-nodes-docker
-    docker-compose -f docker-compose.yaml up -d
-    echo "Create and join channel for each peer"
-    ./setup.sh
-}
-
-configureWorkload()
-{
-    echo "Configuring workload"
-    case $benchmark in
-    kvstore)
-        cd ~/blockbench/src/macro/kvstore/fabric-v1.4-node
-        npm install
-        ./deploy_kv.sh
-        ;;
-    donothing)
-        cd ~/blockbench/src/macro/kvstore/fabric-v1.4-node
-        npm install
-        ./deploy_donothing.sh
-        ;;
-    smallbank)
-        cd ~/blockbench/src/macro/smallbank/api_adapters/fabric-v1.4-node
-        npm install
-        ./deploy.sh
-        ;;
-    cpuheavy)
-        cd ~/blockbench/src/micro
-        npm install
-        cd ~/blockbench/src/micro/cpuheavy/fabric-v1.4
-
-
-        ;;
-    ioheavy)
-        cd ~/blockbench/src/micro
-        npm install
-        cd ~/blockbench/src/micro/ioheavy/fabric-v1.4
-        ;;
-    *)
-        echo "Sorry, could not recognize benchmark"
-        ;;
-esac
-
-}
-
-
-kvstoreFunction()
-{
-    echo ""
-    startNetwork
-
-}
+startNetwork
+configureWorkload

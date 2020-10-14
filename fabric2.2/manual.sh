9ddb6a9d346037822df2ae1756a614d54a51f5e4
David <d.satomi@outlook.de>
2020-10-14 19:57:20 +0200

    change workload

--- fabric2.2/manual.sh
+++ fabric2.2/manual.sh
@@ -7,7 +7,7 @@ leveldb_function()
     i=1
     end=5
 
-    mkdir results/leveldb
+    mkdir results/leveldb2
 
     while [ $i -le $end ]; do
         if [ "$i" -lt "100" ]; then
@@ -15,7 +15,7 @@ leveldb_function()
         else
             txr_out="$i"
         fi
-        ./solofabric.sh -b ycsb -t 100 -T 16 -o "leveldb/$i"_"leveldb" -s 300 -w workloada.spec
+        ./solofabric.sh -b ycsb -t 100 -T 16 -o "leveldb2/$i"_"leveldb2" -s 300 -w rw_d.spec
         i=$(($i+$interval))
     done 
 }
@@ -26,7 +26,7 @@ couchdb_function()
     i=1
     end=5
 
-    mkdir results/couchdb
+    mkdir results/couchdb2
 
     while [ $i -le $end ]; do
         if [ "$i" -lt "100" ]; then
@@ -34,7 +34,7 @@ couchdb_function()
         else
             txr_out="$i"
         fi
-        ./solofabric.sh -b ycsb -t 100 -T 16 -o "couchdb/$i"_"couchdb" -s 300 -w workloada.spec -d couchdb
+        ./solofabric.sh -b ycsb -t 100 -T 16 -o "couchdb2/$i"_"couchdb2" -s 300 -w rw_d.spec -d couchdb
         i=$(($i+$interval))
     done 
 }

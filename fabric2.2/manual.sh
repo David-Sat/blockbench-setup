1500d552edbf39c367fb185cd4b13cf804f35911
David <d.satomi@outlook.de>
2020-10-15 11:19:02 +0200

    smallbank

--- fabric2.2/manual.sh
+++ fabric2.2/manual.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-leveldb_function()
+ycsb_leveldb_function()
 {
     interval=1
     i=1
@@ -20,7 +20,7 @@ leveldb_function()
     done 
 }
 
-couchdb_function()
+ycsb_couchdb_function()
 {
     interval=1
     i=1
@@ -39,6 +39,49 @@ couchdb_function()
     done 
 }
 
-leveldb_function
+smallbank_couchdb_function()
+{
+    interval=1
+    i=1
+    end=5
 
-couchdb_function
+    mkdir results/smallbank_cb
+
+    while [ $i -le $end ]; do
+        if [ "$i" -lt "100" ]; then
+            txr_out="0""$i"
+        else
+            txr_out="$i"
+        fi
+        ./solofabric.sh -b smallbank -t 100 -T 16 -o "smallbank_cb/$i"_"sm_couchdb" -s 300 -n 10000000 -f stat.txt  -d couchdb
+        i=$(($i+$interval))
+    done 
+}
+
+smallbank_leveldb_function()
+{
+    interval=1
+    i=1
+    end=5
+
+    mkdir results/smallbank_lv
+
+    while [ $i -le $end ]; do
+        if [ "$i" -lt "100" ]; then
+            txr_out="0""$i"
+        else
+            txr_out="$i"
+        fi
+        ./solofabric.sh -b smallbank -t 100 -T 16 -o "smallbank_lv/$i"_"sm_leveldb" -s 300 -n 10000000 -f stat.txt
+        i=$(($i+$interval))
+    done 
+}
+
+
+
+
+#ycsb_leveldb_function
+#ycsb_couchdb_function
+
+smallbank_couchdb_function
+smallbank_leveldb_function

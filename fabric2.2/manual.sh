99a32a02ca5b223fa3a4cb2809cb94be0c5c0e0d
David <d.satomi@outlook.de>
2020-10-26 17:04:18 +0100

    dist

--- fabric2.2/manual.sh
+++ fabric2.2/manual.sh
@@ -110,8 +110,20 @@ zipfian_function()
 #zipfian_function
 #smallbank_leveldb_function
 
-./workloads.sh -b ycsb -T 16 -t 100 -o rc1 -s 300
-./workloads.sh -b ycsb -T 16 -t 100 -o rc2 -s 300
-./workloads.sh -b ycsb -T 16 -t 100 -o rc3 -s 300
-./workloads.sh -b ycsb -T 16 -t 100 -o rc4 -s 300
-./workloads.sh -b ycsb -T 16 -t 100 -o rc5 -s 300
+#./workloads.sh -b ycsb -T 16 -t 100 -o rc1 -s 300
+#./workloads.sh -b ycsb -T 16 -t 100 -o rc2 -s 300
+#./workloads.sh -b ycsb -T 16 -t 100 -o rc3 -s 300
+#./workloads.sh -b ycsb -T 16 -t 100 -o rc4 -s 300
+#./workloads.sh -b ycsb -T 16 -t 100 -o rc5 -s 300
+
+./workloads.sh -b ycsb -T 16 -t 100 -o dist1 -s 300
+./workloads.sh -b ycsb -T 16 -t 100 -o dist2 -s 300
+./workloads.sh -b ycsb -T 16 -t 100 -o dist3 -s 300
+./workloads.sh -b ycsb -T 16 -t 100 -o dist4 -s 300
+./workloads.sh -b ycsb -T 16 -t 100 -o dist5 -s 300
+
+
+./msgrange.sh -b ycsb -T 16 -o msgrange2 -s 300 -w workloada.spec
+./msgrange.sh -b ycsb -T 16 -o msgrange3 -s 300 -w workloada.spec
+
+
